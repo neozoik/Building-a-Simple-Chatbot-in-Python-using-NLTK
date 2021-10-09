f9ed15b9c4df4a550ad355731d0a60202c2f0a06
Aliaksei Rubanau <alex.rubanov@gmail.com>
2021-10-09 05:36:11 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,4 @@
 
-#Meet Robo: your friend
-
 #import necessary libraries
 import io
 import random
@@ -69,24 +67,24 @@ def response(user_response):
 
 
 flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+print("mBRAIN: My name is mBRAIN. I will answer your queries about Medical Brain. If you want to exit, type Bye!")
 while(flag==True):
     user_response = input()
     user_response=user_response.lower()
     if(user_response!='bye'):
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
-            print("ROBO: You are welcome..")
+            print("mBRAIN: You are welcome..")
         else:
             if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
+                print("mBRAIN: "+greeting(user_response))
             else:
-                print("ROBO: ",end="")
+                print("mBRAIN: ",end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
         flag=False
-        print("ROBO: Bye! take care..")    
+        print("mBRAIN: Bye! take care..")    
         
         
 
